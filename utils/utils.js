461c8105e2af295e1a71afa6d547d654f30b0945
Jean Pierre Habinshuti <hajepierre@gmail.com>
2020-01-15 23:15:55 +0200

    finished writing all required functions

--- utils/utils.js
+++ utils/utils.js
@@ -5,6 +5,8 @@ const {
   isValidRoman
 } = require("roman-numbers");
 
+const math = require("mathjs");
+
 const specialCharacters = [
   "(",
   ")",
@@ -20,46 +22,56 @@ const specialCharacters = [
   "."
 ];
 
-const isSpecialCharacter = value => {
-  return specialCharacters.includes(value);
-};
 
 const getContainedSpecialCharacters = text => {
   const result = [];
-  for (let c of specialCharacters) {
-    if (text.includes(c)) {
+  const textChars = text.split("");
+
+  for (let c of textChars) {
+    if (specialCharacters.includes(c)) {
       result.push(c);
     }
   }
   return result;
 };
 
-// const containsSpecialCharacters = (text, chars = specialCharacters) => {
-//   const result = false;
-//   for (let c of chars) {
-//     if (text.includes(c)) {
-//       result = true;
-//       break;
-//     }
-//   }
-//   return result;
-// };
+
+const tokenize = (text, target) => {
+  const index = text.indexOf(target);
+  let result = [];
+  const firstPart = text.slice(0, index);
+  let secondPart = "";
+
+  if (firstPart) {
+    result.push(firstPart);
+  }
+
+  result.push(target);
+  if (index + 1 < text.length) {
+    secondPart = text.slice(index + 1, text.length);
+    if (secondPart.includes(target)) {
+      result = result.concat(tokenize(secondPart, target));
+    } else {
+      result.push(secondPart);
+    }
+  }
+
+  return result;
+};
 
 const romanToMathExpression = text => {
   //Find contained special characters in the text first
   const symbols = getContainedSpecialCharacters(text);
   if (symbols.length > 0) {
     let result = "";
-    for (let c of symbols) {
-      //Tokenize based on the found special characters
-      const splits = text.split(c);
-      //Loop through all obtained splits to create a math expression
-      for (let s of splits) {
-        if (isSpecialCharacter(s)) {
-          result += s;
-        } else {
-          result += romanToMathExpression(s);
-        }
+    const specialChar = symbols[0];
+    const tokens = tokenize(text, specialChar);
+    console.log(tokens);
+    for (let t of tokens) {
+      if (t === specialChar) {
+        result += t;
+      } else {
+        result += romanToMathExpression(t);
       }
     }
     return result;
@@ -81,5 +93,15 @@ computeRomanExpression = text => {
   text = replaceAll(text, " ");
 
   //Put everything to upper case before continuing
-  text=text.toUpperCase();
+  text = text.toUpperCase();
+
+  //Get Math expression
+  const mathExpression = romanToMathExpression(text);
+  //Evaluation the math expression
+  const result = math.evaluate(mathExpression);
+
+  //Return roman result
+  return arabToRoman(result);
 };
+
+console.log(computeRomanExpression("(XXIV + XI)"));
