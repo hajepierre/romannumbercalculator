0ea9770ceb384398aee75dc7a2f7a6f489cc0c89
Jean Pierre Habinshuti <hajepierre@gmail.com>
2020-01-16 15:41:04 +0200

    added web page and dockerfile

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const { computeRomanExpression } = require("../utils/utils");
+const title = "CALCULATOR";
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res) {
+  res.render("index", { title, calc: { result: "XLV" } });
+});
+
+router.post("/", (req, res) => {
+  console.log(req.body.expression);
+  const result = computeRomanExpression(req.body.expression);
+  res.render("index", { title, calc: { result } });
 });
 
 module.exports = router;
